9ff9edaab44420c0468f3c7733c3ff55b297bb7e
Kevin VALERIO <valerio.kevin83@gmail.com>
2019-04-02 10:30:01 +0200

    On affiche les bonnes données dans le dashboard

--- src/app/students/student-informations/student-informations.component.ts
+++ src/app/students/student-informations/student-informations.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Student} from '../../../models/student';
 
 @Component({
   selector: 'app-student-informations',
@@ -6,8 +7,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./student-informations.component.css']
 })
 export class StudentInformationsComponent implements OnInit {
+  @Input()
+  student: Student;
 
   constructor() {
+    this.student = new Student(localStorage.getItem('User'));
   }
 
   ngOnInit() {
