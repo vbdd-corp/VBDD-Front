ef8094b814a24363b5981461ec0bc3e238e9ec6d
Kevin VALERIO <valerio.kevin83@gmail.com>
2019-04-09 10:53:53 +0200

    Push des miffs

--- src/app/modules/modules-manager/modules-manager.component.ts
+++ src/app/modules/modules-manager/modules-manager.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {first} from 'rxjs/operators';
+import {ModuleService} from '../../../services/module.service';
 
 @Component({
   selector: 'app-modules-manager',
@@ -6,11 +8,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./modules-manager.component.css']
 })
 export class ModulesManagerComponent implements OnInit {
+  modules: any;
+  @Input() reportType: number;
 
-  constructor() {
+  constructor(private moduleService: ModuleService) {
   }
 
+
   ngOnInit() {
+  }
+
+  putModulesInDom() {
+    this.moduleService.getModules(this.reportType)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.addEachModuleToDom(JSON.stringify(data));
+        },
+        error => {
+          alert(error.error.error);
+        });
+  }
+
+  private addEachModuleToDom(data: string) {
+    let parsedModule = JSON.parse(data);
+    for (let i in parsedModule) {
+      this.modules.push({dossier: parsedModule[i], name: parsedModule[i].name});
+    }
   }
 
 }
