3b5872c943eb149c5ddfeebb62c86b38d275d807
Jean-Marie Dormoy <jean-marie.dormoy@etu.univ-cotedazur.fr>
2019-04-30 16:55:13 +0200

    more work on student-informations component.

--- src/app/students/student-informations/student-informations.component.ts
+++ src/app/students/student-informations/student-informations.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Inject, OnInit} from '@angular/core';
 import {Student} from '../../../models/student';
 import {Utils} from '../../../models/utils';
+import {element} from 'protractor';
 
 
 @Component({
@@ -11,15 +12,35 @@ import {Utils} from '../../../models/utils';
 export class StudentInformationsComponent implements OnInit {
   student: Student;
   isStudent: boolean;
+  onEditedMode: boolean = false;
+  elementRef: ElementRef;
 
-  constructor() {
+  constructor(@Inject(ElementRef) elementRef: ElementRef) {
+    this.elementRef = elementRef;
     this.student = Utils.getUser();
     this.isStudent = Utils.isStudent();
     console.log('this.student => ', this.student);
     console.log('this.isStudent => ', this.isStudent);
   }
 
+
   ngOnInit() {
+  }
+
+  toggle() {
+    this.onEditedMode = !this.onEditedMode;
+    let tdList = this.elementRef.nativeElement.querySelectorAll('td');
+    if (this.onEditedMode) {
+      for (let i = 0; i < tdList.length; ++i) {
+        tdList[i].setAttribute('contentEditable', true);
+        tdList[i].style.border = '2px solid #5BC0DE';
+      }
+    } else {
+      for (let i = 0; i < tdList.length; ++i) {
+        tdList[i].setAttribute('contentEditable', false);
+        tdList[i].style.border = '1px solid #ddd';
+      }
+    }
   }
 
 }
