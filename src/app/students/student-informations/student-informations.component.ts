838b8d8de919ad56bbad482795d8a1b546107a37
Jean-Marie Dormoy <jean-marie.dormoy@etu.univ-cotedazur.fr>
2019-05-01 16:45:16 +0200

    more work on student-informations component.

--- src/app/students/student-informations/student-informations.component.ts
+++ src/app/students/student-informations/student-informations.component.ts
@@ -1,8 +1,9 @@
-import {Component, ElementRef, Inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Student} from '../../../models/student';
 import {Utils} from '../../../models/utils';
 import {StudentService} from '../../../services/student.service';
 import {element} from 'protractor';
+import {add} from 'ngx-bootstrap/chronos';
 
 
 @Component({
@@ -14,14 +15,12 @@ export class StudentInformationsComponent implements OnInit {
   student: Student;
   isStudent: boolean;
   onEditedMode: boolean = false;
-  elementRef: ElementRef;
 
-  constructor(@Inject(ElementRef) elementRef: ElementRef, private studentService: StudentService) {
-    this.elementRef = elementRef;
+  constructor(private studentService: StudentService) {
     this.student = Utils.getUser();
     this.isStudent = Utils.isStudent();
-    console.log('this.student => ', this.student);
-    console.log('this.isStudent => ', this.isStudent);
+    // console.log('this.student => ', this.student);
+    // console.log('this.isStudent => ', this.isStudent);
   }
 
 
@@ -30,40 +29,56 @@ export class StudentInformationsComponent implements OnInit {
 
   toggle() {
     this.onEditedMode = !this.onEditedMode;
-    let tdList = this.elementRef.nativeElement.querySelectorAll('td');
+    let tdList = document.querySelectorAll('div.info-container td') as NodeListOf<HTMLElement>;
     if (this.onEditedMode) {
       for (let i = 0; i < tdList.length; ++i) {
-        tdList[i].setAttribute('contentEditable', true);
+        tdList[i].setAttribute('contentEditable', 'true');
         tdList[i].style.border = '2px solid #5BC0DE';
       }
     } else {
       for (let i = 0; i < tdList.length; ++i) {
-        tdList[i].setAttribute('contentEditable', false);
+        tdList[i].setAttribute('contentEditable', 'false');
         tdList[i].style.border = '1px solid #ddd';
+        // tdList[i].setAttribute(   'border', '1px solid #ddd');
       }
     }
   }
 
   saveModifications() {
     this.toggle();
-    let mail = this.elementRef.nativeElement.get
-    /*let stud = {
-      "mail": null,
-      "firstName": "Coco",
-      "lastName": "Martin",
-      "major": "SI3",
-      "gender": "M",
-      "nationality": "FR",
-      "birthDate": "22/08/1997",
-      "INE": "2379263974T",
-      "studentNumber": "21560703",
-      "address": "1 Avenue G.Pompidou, Residence des Dolines, APP 256",
-      "city": "Valbonne",
-      "postalCode": "06560",
-      "mobilePhoneNumber": "0605468955",
-      "phoneNumber": "0455637198",
+    let mail = document.getElementById('mail').innerText;
+    let firstName = document.getElementById('firstName').innerText;
+    let lastName = document.getElementById('lastName').innerText;
+    let major = document.getElementById('major').innerText;
+    let gender = document.getElementById('gender').innerText;
+    let nationality = document.getElementById('nationality').innerText;
+    let birthDate = document.getElementById('birthDate').innerText;
+    let INE = document.getElementById('INE').innerText;
+    let studentNumber = document.getElementById('studentNumber').innerText;
+    let address = document.getElementById('address').innerText;
+    let city = document.getElementById('city').innerText;
+    let postalCode = document.getElementById('postalCode').innerText;
+    let mobilePhoneNumber = document.getElementById('mobilePhoneNumber').innerText;
+    let phoneNumber = document.getElementById('phoneNumber').innerText;
+
+    let stud = {
+      'mail': mail,
+      'firstName': firstName,
+      'lastName': lastName,
+      'major': major,
+      'gender': gender,
+      'nationality': nationality,
+      'birthDate': birthDate,
+      'INE': INE,
+      'studentNumber': studentNumber,
+      'address': address,
+      'city': city,
+      'postalCode': postalCode,
+      'mobilePhoneNumber': mobilePhoneNumber,
+      'phoneNumber': phoneNumber
     };
 
-    this.studentService.updateStudent()*/
+    console.log('stud ==> ', stud);
+    /* this.studentService.updateStudent()*/
   }
 }
