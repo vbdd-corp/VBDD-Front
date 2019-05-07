d3b1aae8a463e7a0a84829ca020324ce99449848
Kevin VALERIO <valerio.kevin83@gmail.com>
2019-05-07 22:44:44 +0200

    Download file is now ok. Need to show informations for BRI while looking files now

--- src/services/download.service.ts
+++ src/services/download.service.ts
@@ -1,28 +1,37 @@
 import {Injectable} from '@angular/core';
-import {httpOptionsBase, serverUrl} from '../config/server.config';
-import {HttpClient} from '@angular/common/http';
+import {serverUrl} from '../config/server.config';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {saveAs} from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DownloadService {
 
-  private httpOptions = httpOptionsBase;
 
   constructor(private http: HttpClient) {
   }
 
-  getFile(link): any {
-
-    return this.http.post(serverUrl + '/api/module/download', {filePath: link}, this.httpOptions)
-     // TODO: ne compile pas !!!
-    //  .map((response: Response) => response.json());
+  public downloadFile(link) {
+    this.downloadReport(link).subscribe(
+      data => {
+        saveAs(data, link);
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
-  public downloadFile(response) {
-    const blob = new Blob([response], {type: 'text/csv'});
-    const url = window.URL.createObjectURL(blob);
-    window.open(url);
-  }
+  private downloadReport(link): Observable<any> {
 
+    var body = {filePath: link};
+
+    return this.http.post(serverUrl + '/api/module/download', body, {
+      responseType: 'blob',
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+
+  }
 }
