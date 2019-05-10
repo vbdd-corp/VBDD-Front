9f2e4e2ffb154ba518ad5bf23f933c5d18c20688
Kevin VALERIO <valerio.kevin83@gmail.com>
2019-05-10 22:39:36 +0200

    Filter is coming more than even

--- src/app/bri/reports-filterer/reports-filterer.component.ts
+++ src/app/bri/reports-filterer/reports-filterer.component.ts
@@ -3,6 +3,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AlertService} from '../../../services';
 import {DossierService} from '../../../services/dossier.service';
 import {Subscription} from 'rxjs';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-reports-filterer',
@@ -13,13 +15,13 @@ export class ReportsFiltererComponent implements OnInit {
 
   filterForm: FormGroup;
   dossiers: Array<any>;
+  initialDossiers: Array<any>;
   sub: Subscription;
   private filtersList = ['ecole', 'cursus', 'pays', 'etudiant'];
 
   constructor(private formBuilder: FormBuilder, private alertService: AlertService, private dossierService: DossierService) {
     this.dossierService.getAllFiles();
     this.sub = this.dossierService.files$.subscribe(files => this.dossiers = files);
-
   }
 
   get f() {
@@ -33,7 +35,9 @@ export class ReportsFiltererComponent implements OnInit {
   }
 
   downloadAsCSV() {
-
+    var doc = new jsPDF();
+    doc.autoTable({html: '#mainTable'});
+    doc.save('dossiers' + Math.floor(Math.random() * Math.floor(999)) + '.pdf');
   }
 
   splitFilterInput() {
@@ -49,9 +53,8 @@ export class ReportsFiltererComponent implements OnInit {
     }
   }
 
-  private filterTable(filter: string, value: string) {
-    console.log('Filtre : ' + filter + ' | Valeur : ' + value);
-
+  reset() {
+    this.dossiers = this.initialDossiers;
   }
 
   private isValid(filter) {
@@ -59,5 +62,38 @@ export class ReportsFiltererComponent implements OnInit {
       return false;
     }
     return !(this.filtersList.indexOf(filter) <= -1);
+  }
+
+  private filterTable(filter: string, value: string) {
+    console.log('Filtre : ' + filter + ' | Valeur : ' + value);
+
+    switch (filter) {
+      case this.filtersList[0]:
+        //ecole
+
+
+        break;
+      case this.filtersList[1]:
+        //cursus
+
+        break;
+      case this.filtersList[2]:
+        //pays
+
+        break;
+      case this.filtersList[3]:
+        //etudiant
+        const fullName = value.split(' ');
+        let nom = fullName[0];
+        let prenom = fullName[1];
+
+        for (let i = 0; i < this.dossiers.length; i++) {
+          if (!(this.dossiers[i].student.firstName.toLowerCase() == prenom.toLowerCase() && this.dossiers[i].student.lastName.toLowerCase() == nom.toLowerCase())) {
+            this.dossiers.splice(i, 1);
+          }
+        }
+
+        break;
+    }
   }
 }
