b51e715ff8d1f280d85bf0f33467834450991285
Jean-Marie Dormoy <jean-marie.dormoy@etu.univ-cotedazur.fr>
2019-05-21 10:11:10 +0200

    contrat-etude component WIP Total ECTS.

--- src/app/modules/list-modules/contrat-etude/contrat-etude.component.ts
+++ src/app/modules/list-modules/contrat-etude/contrat-etude.component.ts
@@ -29,6 +29,8 @@ export class ContratEtudeComponent implements OnInit, AfterViewInit {
   @Input() file: File;
   @ViewChild('togglePrintemps') private elTogglePrintemps;
   @ViewChild('toggleAutomne') private elToggleAutomne;
+  @ViewChild('totalECTSS2') private elTotalECTSS2;
+  @ViewChild('totalECTSS1') private elTotalECTSS1;
   isValidated: boolean = false;
   public schoolSelected: School;
 
@@ -112,11 +114,26 @@ export class ContratEtudeComponent implements OnInit, AfterViewInit {
     tdOfSelectedModule.forEach(td => td.classList.remove('selected'));
   }
 
+  updateTotalECTS(semester) {
+    let sum = 0;
+    let val;
+    for (let i = 1; i <= 12; i++) {
+      val = parseInt(
+        this.f[semester + 'nombreCredits' + i].value,
+        10);
+      if (!isNaN(val))
+        sum += val;
+    }
+    if (semester === 's1')
+      this.elTotalECTSS1.nativeElement.textContent = 'Total ECTS :   ' + sum;
+    else if (semester === 's2')
+      this.elTotalECTSS2.nativeElement.textContent = 'Total ECTS :   ' + sum;
+  }
+
   constructor(
     private formBuilder: FormBuilder,
     private moduleService: ModuleService,
-    private schoolService: SchoolService,
-    private changeDetector: ChangeDetectorRef) {
+    private schoolService: SchoolService) {
 
     this.sub1 = this.schoolService.school1$.subscribe(school => {
       if (Object.keys(this.choice1).length > 0) {
